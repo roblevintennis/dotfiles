6d6e26fe9605429182753f893712a8c4f8acf378
Rob Levin <rlevin@sugarcrm.com>
2012-05-02 13:32:45 -0700

    changed sugarbuild

--- bin/sugarbuild.sh
+++ bin/sugarbuild.sh
@@ -2,11 +2,18 @@
 
 pushd ~/programming/sugar/Mango/build/rome > /dev/null 2>&1
 
+echo "Building sugarcrm..."
+
 date >> /Users/rlevin/bin/log/sugarbuild.log 2>&1
 
 #echo "Input file is: ${1}" >> $HOME/bin/log/sugarbuild.log 2>&1
 
 /usr/bin/php build.php --ver=6.5.0 --flav=ent --build_dir=/Applications/MAMP/htdocs --dir=$1 > /dev/null 2>&1
 
+echo "Copying config.php file over..."
+cp /Users/rlevin/programming/sugar/config_GOOD.php /Applications/MAMP/htdocs/ent/sugarcrm/config.php
+
 popd > /dev/null 2>&1
+
+echo "Done"
 
